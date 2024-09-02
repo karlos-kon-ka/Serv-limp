c62c4f6d62c135e5f0ad37514b67e09d93bf9fe0
Dani <carlos99.cp88secundaria@gmail.com>
2024-09-02 16:58:54 +0200

    third

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { AppBar, Toolbar, Button, Box } from '@mui/material';
 
@@ -6,6 +5,9 @@ function Nav() {
   return (
     <AppBar position="static" color="primary">
       <Toolbar>
+        
+        <img src="/img/productos.png" alt="Logo" style={{ height: 40, marginRight: 16 }} />
+
         <Box sx={{ flexGrow: 1, display: 'flex', gap: 2 }}>
           <Button component={NavLink} to="/" id='enlace' color="inherit" sx={{ textTransform: 'none' }}>
             Inicio
